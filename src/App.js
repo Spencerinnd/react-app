a6f8fd65b55d900b7b198bfaf97768cbf50880a9
Spencer Cook <Spencerinnd@gmail.com>
2021-09-12 01:04:13 -0500

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import logo from './img/AVI.png';
 import './App.css';
+
+import './App.css';
+import AVI from "./img/AVI.png";
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>My React App</h1>
+      <h2>Author: Spencer Cook</h2>
+      <img src={logo} alt="Could not display AVI" />
     </div>
   );
 }
